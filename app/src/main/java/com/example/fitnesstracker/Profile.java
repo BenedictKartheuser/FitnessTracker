367515ea8d5b290b95e4cc260b3d2816a1b1b818
Benedict Kartheuser <kartbene@gmx.de>
2019-10-14 17:06:45 +0200

    Added Profile, Sport and Workout class

---
+++ app/src/main/java/com/example/fitnesstracker/Profile.java
@@ -0,0 +1,37 @@
+package com.example.fitnesstracker;
+
+public class Profile {
+
+    private String name;
+    private int size;
+    private int weigth;
+
+    public Profile() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getWeigth() {
+        return weigth;
+    }
+
+    public void setWeigth(int weigth) {
+        this.weigth = weigth;
+    }
+
+}
